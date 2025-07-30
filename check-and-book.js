62d5ecb53e7c6b76438960538ebfab4ae7ee64d6
fhasani <fjolla.h14@gmail.com>
2025-07-30 12:58:13 +0200

    add workflow fix

--- check-and-book.js
+++ check-and-book.js
@@ -3,8 +3,8 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({
     headless: true,
+    executablePath: '/usr/bin/google-chrome-stable',
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    defaultViewport: null,
   });
 
   const page = await browser.newPage();
